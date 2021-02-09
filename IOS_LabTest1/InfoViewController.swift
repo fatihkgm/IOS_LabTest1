b8d1d3c5d8dad9102a1d642af1da3a1a1e419f10
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-09 01:26:48 -0500

    SecondPage implement

---
+++ IOS_LabTest1/InfoViewController.swift
@@ -0,0 +1,30 @@
+//
+//  InfoViewController.swift
+//  IOS_LabTest1
+//
+//  Created by Fatih on 2021-02-09.
+//
+
+import UIKit
+
+class InfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = " Who I Am"
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
