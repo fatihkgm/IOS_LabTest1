efc56776d407ef11d453a6f04796d19a75e91cff
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-09 00:54:20 -0500

    Initial Commit

---
+++ IOS_LabTest1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IOS_LabTest1
+//
+//  Created by Fatih on 2021-02-09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
