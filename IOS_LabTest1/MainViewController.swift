b973199c3a0dc7c5346e73b2b07a47691182ad35
fatih kgokmen <fatih.kgokmen@gmail.com>
2021-02-09 02:58:32 -0500

    function n1 work

--- IOS_LabTest1/MainViewController.swift
+++ IOS_LabTest1/MainViewController.swift
@@ -6,25 +6,39 @@
 //
 
 import UIKit
+import GameKit
+
 
 class MainViewController: UIViewController {
+    
+    var numbers = [Int](1...65)
+    
+    var randomArray = [Int]()
+    
 
+    @IBOutlet weak var fristNumber: UILabel!
+    @IBOutlet weak var secondNumber: UILabel!
+    @IBOutlet weak var thirdNumber: UILabel!
+    @IBOutlet weak var fouthNumber: UILabel!
+    @IBOutlet weak var fifthNuber: UILabel!
+    @IBOutlet weak var sixtNumber: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Lab Test 1"
 
-        // Do any additional setup after loading the view.
     }
     
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func drawBtn(_ sender: UIButton) {
+        createNumbers()
     }
-    */
+    
+    func createNumbers(){
+        randomArray = GKRandomSource.sharedRandom().arrayByShufflingObjects(in: numbers) as! [Int]
+        fristNumber.text = String(randomArray[0])
+    }
+    
 
 }
